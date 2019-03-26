0492eb84458b5ced37aa350793910036e334a3aa
Pierpaolo Calanna <p.calanna@gmail.com>
2019-03-26 14:33:02 +0100

    added download post article on the fly

--- flaskApp/extensions/jinja_ext.py
+++ flaskApp/extensions/jinja_ext.py
@@ -1,5 +1,6 @@
 
 import mistune
+from base64 import b64encode
 
 # init markdown
 markdown = mistune.Markdown()
@@ -11,7 +12,12 @@ def format_datetime(value, format="%d %b %Y %I:%M %p"):
 def md_to_html(value):
   return markdown(value)
 
+def btoa(value):
+  if value is None: return ""
+  return b64encode(bytes(value, 'utf-8')).decode("utf-8")
+
 # attacher
 def attach_jinja(app):
   app.jinja_env.filters['to_data_time'] = format_datetime
   app.jinja_env.filters['md_to_html'] = md_to_html
+  app.jinja_env.filters['b64_encode'] = btoa
