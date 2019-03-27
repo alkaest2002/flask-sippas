caf3e9f01eca678abd44ea34c2a031d68942649c
Pierpaolo Calanna <p.calanna@gmail.com>
2019-03-27 08:04:16 +0100

    update static pages

--- flaskApp/blueprints/main/routes.py
+++ flaskApp/blueprints/main/routes.py
@@ -10,7 +10,7 @@ from . import bp_main
 @cache.cached()
 def index():
 
-  # fetch latest articles from blog
+  # fetch stickies & latest articles from blog
   stickies = query_db('SELECT * FROM posts WHERE is_sticky == 1 ORDER BY id DESC LIMIT 2')
   latest = query_db('SELECT * FROM posts  WHERE is_sticky == 0 ORDER BY id DESC LIMIT {}'.format(4 if len(stickies) < 2 else 7))
 
@@ -18,15 +18,15 @@ def index():
   return render_template("main/index.html", latests=latest, stickies=stickies )
 
 @bp_main.route("/services")
-@cache.cached()
+@cache.cached(timeout=60*60*24)
 def services():
-
+  
   # render view
   return render_template("main/services.html")
 
 @bp_main.route("/gild")
-@cache.cached()
+@cache.cached(timeout=60*60*24)
 def gild():
-
+  
   # render view
   return render_template("main/gild.html")
