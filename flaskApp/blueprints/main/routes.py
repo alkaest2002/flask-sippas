2075d813c6315a8994436d42a0747f0670b16173
Pierpaolo Calanna <p.calanna@gmail.com>
2019-03-28 12:59:15 +0100

    added cooki consent

--- flaskApp/blueprints/main/routes.py
+++ flaskApp/blueprints/main/routes.py
@@ -1,10 +1,14 @@
 
-from flask import render_template
+from flask import render_template, request
 
 from flaskApp.db.sqlite import query_db
 from flaskApp.extensions.cache_ext import cache
 
 from . import bp_main
+
+# ################################################################################
+# ROUTES 
+# ################################################################################
 
 @bp_main.route("/")
 @cache.cached()
@@ -15,7 +19,7 @@ def index():
   latest = query_db('SELECT * FROM posts  WHERE is_sticky == 0 ORDER BY id DESC LIMIT {}'.format(4 if len(stickies) < 2 else 7))
   
   # render view
-  return render_template("main/index.html", latests=latest, stickies=stickies )
+  return render_template("main/index.html", latests=latest, stickies=stickies, consent = request.cookies.get('cookie_consent'))
 
 @bp_main.route("/services")
 @cache.cached(timeout=60*60*24)
