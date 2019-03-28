480bdccef02793cc9615e6a83f6c05a47c18d2a5
Pierpaolo Calanna <p.calanna@gmail.com>
2019-03-28 15:23:23 +0100

    refactored cookie consent

--- flaskApp/blueprints/main/routes.py
+++ flaskApp/blueprints/main/routes.py
@@ -1,5 +1,5 @@
 
-from flask import render_template, request
+from flask import render_template
 
 from flaskApp.db.sqlite import query_db
 from flaskApp.extensions.cache_ext import cache
@@ -19,7 +19,7 @@ def index():
   latest = query_db('SELECT * FROM posts  WHERE is_sticky == 0 ORDER BY id DESC LIMIT {}'.format(4 if len(stickies) < 2 else 7))
   
   # render view
-  return render_template("main/index.html", latests=latest, stickies=stickies, consent = request.cookies.get('cookie_consent'))
+  return render_template("main/index.html", latests=latest, stickies=stickies)
 
 @bp_main.route("/services")
 @cache.cached(timeout=60*60*24)
