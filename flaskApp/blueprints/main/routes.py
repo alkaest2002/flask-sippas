50ee5871b02b093ae2dec3c38caa583bf24167e5
Pierpaolo Calanna <p.calanna@gmail.com>
2019-04-04 13:46:53 +0200

    code update in main blueprint

--- flaskApp/blueprints/main/routes.py
+++ flaskApp/blueprints/main/routes.py
@@ -1,5 +1,5 @@
 
-from flask import render_template
+from flask import render_template, request
 
 from flaskApp.db.sqlite import query_db
 from flaskApp.extensions.cache_ext import cache
@@ -16,10 +16,13 @@ def index():
 
   # fetch stickies & latest articles from blog
   stickies = query_db('SELECT * FROM posts WHERE is_sticky == 1 ORDER BY updated_at DESC LIMIT 2')
-  latest = query_db(f'SELECT * FROM posts  WHERE is_sticky == 0 ORDER BY id DESC LIMIT {4 if len(stickies) < 2 else 7}')
+  latests = query_db(f'SELECT * FROM posts  WHERE is_sticky == 0 ORDER BY id DESC LIMIT {4 if len(stickies) < 2 else 7}')
   
+  # show consent
+  show_cookie_consent = False if request.cookies.get('sippas_cookie_consent') else True
+
   # render view
-  return render_template("main/index.html", latests=latest, stickies=stickies)
+  return render_template("main/index.html", latests=latests, stickies=stickies, show_cookie_consent=show_cookie_consent)
 
 @bp_main.route("/services")
 @cache.cached(timeout=60*60*24*30)
